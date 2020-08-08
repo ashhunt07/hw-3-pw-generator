de96cd8aaaa7ea79d345234b46980ceee1c071cd
Ashley Hunt <ashhunt07@gmail.com>
2020-08-07 23:03:06 -0500

    Created initial loop for password length and added all prompts needed.

--- script.js
+++ script.js
@@ -2,51 +2,12 @@
 
 
 //Onclick function to activate generator
-
-
-// Set all password variables
-var allChar = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!?.@#$%&*+_-"
-
-
-decrementEl.addEventListener("click", function() {
-  if(count > 0) {
-    count--;
-    setCounterText();
-  }
-}); 
-
-
-// Prompt to decide password length
-var howManyChar = prompt("How many characters would you like your password to be? (choose a number between 8 and 128 characters)");
-  // Alert the results
-  alert (howManyChar);
-
-
-
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN prompted for character types to include in the password
-// THEN I choose lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
-
-
-
-
-
-
-
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
 
 // Write password to the #password input
 function writePassword() {
@@ -57,11 +18,43 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+
+/* Set all password variables
+var allChar = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!?.@#$%&*+_-"
+var char = allChar.split(" ");*/ 
+
+
+//All variable as separate arrays
+// Various Arrays 
+var num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var aLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var aUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var specChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+
+// Variable Declaration 
+var howManyChar;
+var password = "";
 
 
 
+// Prompt to decide password length
+function generatePassword (){
+  var howManyChar = prompt("How many characters would you like your password to be? (choose a number between 8 and 128 characters)");
+
+  if (howManyChar <8 || howManyChar > 128) {
+    alert("Password length much must be between 8 and 128 charcters. Please try again!");
+    generatePassword();
+  }
+
+// Promt for all other character types to be included in password
+var num = confirm ("you want numbers in your password?");
+var aLower = confirm ("you want Uppercases in your password?");
+var aUpper = confirm ("you want lowercases in your password?");
+var specChar = confirm ("you want special characters in your password?");
+
+
+}
 
 
 
